38cf9f46511e93999377e363ca3a10aac6308869
auiog413 <lbzqh156@gmail.com>
2014-02-13 10:50:26 +0800

    ..

--- application/config/production_anysrv/database.php
+++ application/config/production_anysrv/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = '127.0.0.1';
 $db['default']['username'] = '';
 $db['default']['password'] = '';
-$db['default']['database'] = 'feedback';
+$db['default']['database'] = 'installer_feed';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
